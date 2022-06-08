a8a78edc86457b2df2c43a2128b937308b631f96
Iain Hay <iainhay@Iains-MacBook-Air.local>
2022-06-08 21:03:07 +0100

    initial commit' '

---
+++ models/traveller.js
@@ -0,0 +1,36 @@
+const Traveller = function (journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const results = this.journeys.filter(journey => journey.transport === transport);
+  return results
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const results = this.journeys.filter(journey => journey.distance >= minDistance);
+  return results
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
